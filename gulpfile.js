d56650860250c0f3c06dc9325adb457b3d166ac3
powlsn <powls3n@gmail.com>
2019-03-11 01:14:09 +0100

    few things to setup - see todolist

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,33 @@
 "use strict";
 
 const gulp = require("gulp"),
-    browsersync = require("browser-sync").create();
-
-
-gulp.task("default", function () {
-    console.log("juhuu gulp task works");
-});
-
-gulp.task("html", function () {
-    console.log("do something useful");
-});
-
+  browsersync = require("browser-sync").create();
 
 function html(done) {
-    console.log("saved html file and reload the browser");
-    done();
+  console.log("saved html file and reload the browser");
+  done();
 }
 
 // BrowserSync
 function browserSync(done) {
-    browsersync.init({
-        server: {
-            baseDir: "./app/"
-        },
-        port: 3000
-    });
-    done();
+  browsersync.init({
+    server: {
+      baseDir: "./app/"
+    },
+    port: 3000
+  });
+  done();
 }
 
 // BrowserSync Reload (callback)
 function browserSyncReload(done) {
-    browsersync.reload();
-    done();
+  browsersync.reload();
+  done();
 }
 
 // watch files
 function watchFiles() {
-    gulp.watch("./app/index.html", gulp.series(html, browserSyncReload));
+  gulp.watch("./app/index.html", gulp.series(html, browserSyncReload));
 }
 
 // complexe tasks
