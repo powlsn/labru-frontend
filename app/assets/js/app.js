ffdff97b9567c75052f009e5ea548f34cc438b73
powlsn <powls3n@gmail.com>
2019-03-28 20:44:11 +0100

    completed the reveal on scroll feature

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mm = new MobileMenu();
+const galleryItemScroll = new RevealOnScroll($(".card-columns__card"), "60%");
